0828889f8593dedb72859f666ae0903da8df520c
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2023-04-14 12:15:43 +0200

    Change console log msg

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {console.log(req.body); next()});
+app.use('app.js app.use LOG REQUEST BODY AT BEGINNING', (req, res, next) => {console.log(req.body); next()});
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
